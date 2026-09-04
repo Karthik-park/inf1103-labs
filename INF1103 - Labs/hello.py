f32004b1347603a6b3b7cddd77a3ee11f8c3d3f8
Karthik <2604213@sit.singaporetech.edu.sg>
2026-09-04 18:43:35 +0800

    Dynamic Profile

--- INF1103 - Labs/hello.py
+++ INF1103 - Labs/hello.py
@@ -20,24 +20,35 @@
 #print("Followers :",followers)
 
 #Activity 3
-print ("======================")
-print ("Wlecome here")
-print ("My first post!")
-print ("=======================")
+#print ("======================")
+#print ("Wlecome here")
+#print ("My first post!")
+#print ("=======================")
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
+#username = "cool_creator"
+#bio = "Fun Blogger"
+#followers = 100
 
-followers += 50
-print("Day 1:",followers)
+#followers += 50
+#print("Day 1:",followers)
 
-followers += 20
-print("Day 2:", followers)
+#followers += 20
+#print("Day 2:", followers)
 
-followers -=10
-print("Day 3:", followers)
+#followers -=10
+#print("Day 3:", followers)
 
+#print("Username :", username)
+#print("Bio :",bio)
+#print("Followers :",followers)
+
+#Activity 4
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("======================")
 print("Username :", username)
-print("Bio :",bio)
-print("Followers :",followers)
+print("Age :",age)
+print("Category: ", category)
