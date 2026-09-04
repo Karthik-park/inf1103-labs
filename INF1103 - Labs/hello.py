e81c292d366e79b66c593766cec55738c38c2c44
Karthik <2604213@sit.singaporetech.edu.sg>
2026-09-04 18:37:37 +0800

    Added Profile Variables

---
+++ INF1103 - Labs/hello.py
@@ -0,0 +1,20 @@
+
+#Activity 1
+#print ("======================")
+#print ("Wlecome here")
+#print ("My first post!")
+#print ("=======================")
+
+#Activty 2
+print ("======================")
+print ("Wlecome here")
+print ("My first post!")
+print ("=======================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username :", username)
+print("Bio :",bio)
+print("Followers :",followers)
