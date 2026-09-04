31f0849fd25fa134b2ae66f2f4cdbad8878cc65a
Karthik <2604213@sit.singaporetech.edu.sg>
2026-09-04 18:40:29 +0800

    Assignment Variables

--- INF1103 - Labs/hello.py
+++ INF1103 - Labs/hello.py
@@ -6,6 +6,20 @@
 #print ("=======================")
 
 #Activty 2
+#print ("======================")
+#print ("Wlecome here")
+#print ("My first post!")
+#print ("=======================")
+
+#username = "cool_creator"
+#bio = "Fun Blogger"
+#followers = 100
+
+#print("Username :", username)
+#print("Bio :",bio)
+#print("Followers :",followers)
+
+#Activity 3
 print ("======================")
 print ("Wlecome here")
 print ("My first post!")
@@ -14,6 +28,15 @@ print ("=======================")
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
+
+followers += 50
+print("Day 1:",followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -=10
+print("Day 3:", followers)
 
 print("Username :", username)
 print("Bio :",bio)
