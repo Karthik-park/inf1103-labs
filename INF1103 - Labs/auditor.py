a4c0baa238646759dec8aac6dc0cbfba21158745
Karthik <2604213@sit.singaporetech.edu.sg>
2026-09-11 16:26:08 +0800

    Before coding auditor.py

--- INF1103 - Labs/auditor.py
+++ INF1103 - Labs/auditor.py
@@ -1,1 +1,12 @@
-print("Hello World")
+Inventory = 0 
+
+Stock_quantity = str(input ("Enter a stock quantity : "))
+
+while Stock_quantity != 500 :
+    Stock_quantity = str(input ("Enter a stock quantity : "))
+    Stock_quantity=Stock_quantity.lower
+    if str(Stock_quantity)=="quit":
+        break
+    
+
+print("Ended")
