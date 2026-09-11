5c540645d49b1cfc4f05798adf3cbb3fe02e6ea9
Karthik <2604213@sit.singaporetech.edu.sg>
2026-09-11 20:39:37 +0800

    Finished auditor.py

--- INF1103 - Labs/auditor.py
+++ INF1103 - Labs/auditor.py
@@ -1,12 +1,32 @@
-Inventory = 0 
+total_inventory = 0
+failednumber=0
 
-Stock_quantity = str(input ("Enter a stock quantity : "))
 
-while Stock_quantity != 500 :
-    Stock_quantity = str(input ("Enter a stock quantity : "))
-    Stock_quantity=Stock_quantity.lower
-    if str(Stock_quantity)=="quit":
+while True:
+    entry = input("Enter stock quantity: ")
+
+    if entry == "quit":
         break
-    
 
-print("Ended")
+    try:
+        quantity = int(entry)
+    except ValueError:
+        print("Invalid number entered. Try again. ")
+        failednumber += 1
+        continue
+
+    if quantity < 0:
+        print("No negative numbers. Try again.")
+        failednumber += 1
+        continue
+
+    if total_inventory > 500:
+        print("Overstock detected.")
+        
+        break
+    else:
+        total_inventory = total_inventory+quantity
+        print("Total Inventory: " + str(total_inventory))
+
+print("Number of failed attempts is " + str(failednumber))
+print("Total Inventory is " + str(total_inventory))
