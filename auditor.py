e899536cbdf55fc54d6f61c39d49e4b967edcf07
Karthik <2604213@sit.singaporetech.edu.sg>
2026-09-15 23:26:34 +0800

    Initial Project Setup

---
+++ auditor.py
@@ -0,0 +1,30 @@
+total_inventory = 0
+failednumber=0
+
+
+while True:
+    entry = input("Enter stock quantity: ")
+    if entry == "quit":
+        break
+    try:
+        quantity = int(entry)
+    except ValueError:
+        print("Invalid number entered. Try again. ")
+        failednumber += 1
+        continue
+
+    if quantity < 0:
+        print("No negative numbers. Try again.")
+        failednumber += 1
+        continue
+
+    if total_inventory > 500:
+        print("Overstock detected.")
+        
+        break
+    else:
+        total_inventory = total_inventory+quantity
+        print("Total Inventory: " + str(total_inventory))
+
+print("Number of failed attempts is " + str(failednumber))
+print("Total Inventory is " + str(total_inventory))
